78bb6048395d693fb94cb1a083de87c94400a612
Peter Van Eeckhoutte <peter.ve@corelan.be>
2016-07-08 12:48:02 +0200

    update LFH_TakeBack routine

--- LFH_TakeBack/LFH_TakeBack/LFH_TakeBack.cpp
+++ LFH_TakeBack/LFH_TakeBack/LFH_TakeBack.cpp
@@ -9,8 +9,40 @@
 #include "windows.h"
 #include<iostream>
 #include<vector>
+#include<map>
 
 using namespace std;
+
+std::map<int, int> mapAllocData;
+
+void updateMap(int nrtimes)
+{
+	int curtimes;
+	map<int, int>::iterator it = mapAllocData.find(nrtimes);
+	if (it != mapAllocData.end())
+	{
+		// key exists, so increment its value
+		curtimes = (it->second) + 1;
+		mapAllocData[nrtimes] = curtimes;
+	}
+	else
+	{
+		mapAllocData[nrtimes] = 1;
+	}
+}
+
+void printMap()
+{
+	printf("\n");
+	printf("Nr allocs needed   |  Nr times\n");
+	printf("-------------------------------\n");
+	for (std::map<int, int>::iterator it = mapAllocData.begin(); it != mapAllocData.end(); ++it)
+	{
+		printf("       %d               %d \n", it->first, it->second);
+	}
+	printf("\n");
+}
+
 
 int main()
 {
@@ -18,12 +50,16 @@ int main()
 	HANDLE hChunkFreed;
 	HANDLE hDefaultHeap;
 
+	vector<HANDLE> arrChunks;
+
+
 	double takenbacksum;
 	double nrruns;
 	double mintimes;
 	double maxtimes;
 	int nrtries;
 	int nrnotfound;
+	int maxruns;
 	bool wasfound;
 
 	takenbacksum = 0;
@@ -34,8 +70,19 @@ int main()
 
 	hDefaultHeap = GetProcessHeap();
 
-	for (int nrtimes = 0; nrtimes < 2; nrtimes++)
+	maxruns = 100;
+
+	for (int nrtimes = 0; nrtimes < maxruns; nrtimes++)
 	{
+		printf("\nStarting run %d of %d\n", nrtimes + 1, maxruns);
+		// free chunks first
+		printf("1. Free allocations...\n");
+		for (unsigned chunkindex = 0; chunkindex < arrChunks.size(); chunkindex++)
+		{
+			HeapFree(hDefaultHeap, 0, arrChunks[chunkindex]);
+		}
+		arrChunks.clear();
+		printf("2. Cause new allocations...\n");
 		for (int chunksize = 8; chunksize < 0x4000; chunksize += 8)
 		{
 			// activate LFH for this size (only in first run)
@@ -43,7 +90,8 @@ int main()
 			{
 				for (int y = 0; y < 20; y++)
 				{
-					HeapAlloc(hDefaultHeap, 0, chunksize);
+					hChunk = HeapAlloc(hDefaultHeap, 0, chunksize);
+					arrChunks.push_back(hChunk);
 				}
 			}
 			// cause an allocation and free it again
@@ -55,6 +103,7 @@ int main()
 			for (int z = 1; z < 2000; z++)
 			{
 				hChunk = HeapAlloc(hDefaultHeap, 0, chunksize);
+				arrChunks.push_back(hChunk);
 				if (hChunk == hChunkFreed)
 				{
 					takenbacksum = takenbacksum + z;
@@ -77,12 +126,18 @@ int main()
 						maxtimes = z;
 					}
 					wasfound = true;
+					updateMap(z);
 					break;
 				}
 			}
+			if (!wasfound)
+			{
+				nrnotfound += 1;
+			}
+			/*
 			if (wasfound)
 			{
-				printf("Chunksize 0x%08x : object at 0x%p taken back after %d tries\n", chunksize, hChunkFreed, nrtries);
+				printf("%d/%d - Chunksize 0x%08x : object at 0x%p taken back after %d tries\n", nrtimes+1, maxruns, chunksize, hChunkFreed, nrtries);
 				printf("  Average so far: %f\n", (takenbacksum / nrruns));
 				printf("  Min: %f, max: %f\n", mintimes, maxtimes);
 			}
@@ -90,13 +145,14 @@ int main()
 			{
 				nrnotfound += 1;
 			}
+			*/
 		}
-		printf("Number of times object not taken back: %d\n", nrnotfound);
+		printf("\n  Number of times object not taken back: %d\n", nrnotfound);
+		printf("  GlobalAverage so far: %f\n", (takenbacksum / nrruns));
+		printf("  Min: %f, max: %f\n", mintimes, maxtimes);
 	}
 
-	printf("Done, press return to quit\n");
-	cin.ignore();
-
+	printMap();
 
 	return 0;
 
